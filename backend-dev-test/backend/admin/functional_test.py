a4121afd8aa4d87b6dc5822b5299fb2e25cb89b6
Andres Rengifo <amrengifoh@gmail.com>
2023-11-23 18:59:16 -0500

    feat: add functional and unit tests setup

---
+++ backend-dev-test/backend/admin/functional_test.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException
+import unittest
+import time
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        opts = webdriver.FirefoxOptions()
+        opts.add_argument('--headless')
+        self.browser = webdriver.Firefox(options=opts)
+    
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_element = self.browser.find_element(By.TAG_NAME, 'h1')
+        self.assertIn('Your To-Do list', header_element.text)
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(5)
+        rows = self.browser.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')

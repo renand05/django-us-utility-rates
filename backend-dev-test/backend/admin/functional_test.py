ca2aff470070568f61a5b2af79c6b19e8512ae3c
Andres Rengifo <amrengifoh@gmail.com>
2023-11-23 21:18:20 -0500

    feat: add first model

--- backend-dev-test/backend/admin/functional_test.py
+++ backend-dev-test/backend/admin/functional_test.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import StaleElementReferenceException
 import unittest
 import time
 
